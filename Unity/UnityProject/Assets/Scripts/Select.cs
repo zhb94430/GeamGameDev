1832e742ba1576ab73ac0a88564b40be4de74199
Hongbin Zhang <zhan2385@umn.edu>
2015-01-02 22:20:25 +0800

    Simple Mouse controlled object

---
+++ Unity/UnityProject/Assets/Scripts/Select.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Select : MonoBehaviour {
+
+	public float selectSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void FixedUpdate () {
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		//Set up variables for translating the selected object
+		float selectX = mouseX * selectSpeed * Time.deltaTime;
+		float selectZ = mouseY * selectSpeed * Time.deltaTime;
+
+//		transform.Translate (selectX, 0, selectZ);
+
+		//Add code for physics
+//		rigidbody.AddForce (selectX, 0, selectZ);
+		Vector3 displacement = new Vector3 (selectX, 2, selectZ);
+		Vector3 zeroOut = new Vector3 (1, 0, 1);
+		Matrix4x4 zeroOutMatrix = Matrix4x4.Scale (zeroOut);
+
+		rigidbody.MovePosition (zeroOutMatrix.MultiplyPoint3x4(rigidbody.position) + displacement);
+	}
+}
